a3b37d6af88eb99e2a9fdeb48abfe6d8d7ae407d
Su Yan <suyan0830@gmail.com>
2015-01-10 13:37:13 -0800

    make remote path can be optional

--- Dropzone Action/Qiniu.dzbundle/action.rb
+++ Dropzone Action/Qiniu.dzbundle/action.rb
@@ -12,46 +12,48 @@
 # Version: 1.0
 # MinDropzoneVersion: 3.0
 # RubyPath: /usr/bin/ruby
- 
+
 require 'qiniu'
- 
+
 Qiniu.establish_connection! :access_key => ENV['username'],
                             :secret_key => ENV['password']
- 
+
 ENV['http_proxy'] = ""
- 
+
 def dragged
   puts $items.inspect
   local_file = $items[0]
   puts local_file
-  
+
   put_policy = Qiniu::Auth::PutPolicy.new(ENV['server'], File.basename(local_file))
   uptoken = Qiniu::Auth.generate_uptoken(put_policy)
   puts uptoken
-  
+
   $dz.begin("Starting uploading...")
   $dz.determinate(true)
   $dz.percent(10)
 
   # copy to folder
-  Rsync.do_copy([local_file], ENV['remote_path'], false)
+  if (ENV['remote_path'])
+    Rsync.do_copy([local_file], ENV['remote_path'], false)
+  end
 
   $dz.percent(30)
-   
+
   code, result, response_headers = Qiniu::Storage.upload_with_put_policy(
     put_policy,
     local_file
   )
- 
+
   $dz.percent(60)
-  
+
   puts code
   puts result
   puts response_headers
- 
+
   $dz.percent(100)
   $dz.finish("Url has been copied to clipboard.")
-  
+
   if code != 200
     $dz.fail("Error uploading file")
   else
@@ -59,7 +61,7 @@ def dragged
     $dz.url(url)
   end
 end
- 
+
 def clicked
   system("open http://qiniu.com")
 end
